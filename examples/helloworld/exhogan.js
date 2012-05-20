de6c9792d4353c9ed7e9a30bb63cf9fb809eb798
Kalman Speier <kalman.speier@gmail.com>
2012-05-20 12:59:42 +0100

    cname moved to gh-pages branch

--- examples/helloworld/exhogan.js
+++
@@ -1,8 +0,0 @@
-var hogan = require('hogan.js');
-
-exports.compile = function() {
-  var t = hogan.compile.apply(hogan, arguments);
-  return function() {
-    return t.render.apply(t, arguments);
-  }
-}

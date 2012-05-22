6875a5bec51e32a8943485c291bdbf5a4e609f4f
Kalman Speier <kalman.speier@gmail.com>
2012-05-22 18:59:02 +0100

    delete old files

--- skeletons/express/servup.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var up = require('up');
-
-var master = http.createServer().listen(process.env.PORT || 8080);
-var srv = up(master, __dirname + '/server');
-
-process.on('SIGUSR2', function() {
-  srv.reload();
-});
